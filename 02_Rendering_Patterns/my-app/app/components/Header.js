2d7086a79c2446e27c3edb4d984257231d2253de
ShashankShekhar9839 <111214162+ShashankShekhar9839@users.noreply.github.com>
2025-08-24 19:02:41 +0530

    pages created

--- 02_Rendering_Patterns/my-app/app/components/Header.js
+++ 02_Rendering_Patterns/my-app/app/components/Header.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="fixed top-0 w-full bg-white dark:bg-gray-900 shadow-md z-50">
+    <header className=" top-0 w-full bg-white dark:bg-gray-900 shadow-md z-50 h-14">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <div className="flex-shrink-0">
@@ -30,7 +30,7 @@ const Header = () => {
               href="/services"
               className="text-gray-600 hover:text-gray-900 dark:text-gray-300 dark:hover:text-white transition-colors duration-200"
             >
-              Contact
+             Services
             </Link>
             <Link
               href="/blogs"
